8db3958a726da77c903c9f7119a8bb30d0c9a8e9
dagmawi-y <dagmawi.inbox@gmail.com>
2023-07-12 16:05:49 +0300

    basic theming(font and colors)

--- App.tsx
+++ App.tsx
@@ -1,10 +1,13 @@
-import React from 'react';
 import {View, Text} from 'react-native';
+import colors from './src/theme/colors';
+import fonts from './src/theme/fonts';
 
 const App = () => {
   return (
-    <View style={{alignItems: 'center', justifyContent: 'center'}}>
-      <Text>Hello</Text>
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text style={{color: colors.primary, fontSize: fonts.size.xlg}}>
+        Hola Mundo
+      </Text>
     </View>
   );
 };
---
+++ src/theme/colors.ts
@@ -0,0 +1,10 @@
+const colors = {
+  primary: '#405DE6',
+
+  white: '#fff',
+  black: '#000',
+  grey: '#666666',
+  lightgray: '#cccccc',
+};
+
+export default colors;
