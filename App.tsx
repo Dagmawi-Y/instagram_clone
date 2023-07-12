a32b26ef161f6936380fba8b9d2584b016e1482d
dagmawi-y <dagmawi.inbox@gmail.com>
2023-07-12 16:21:36 +0300

    ant design test

--- App.tsx
+++ App.tsx
@@ -1,12 +1,18 @@
 import {View, Text} from 'react-native';
 import colors from './src/theme/colors';
 import fonts from './src/theme/fonts';
+import AntDesign from 'react-native-vector-icons/AntDesign';
 
 const App = () => {
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text style={{color: colors.primary, fontSize: fonts.size.xlg}}>
+      <Text
+        style={{
+          color: colors.primary,
+          fontSize: fonts.size.xlg,
+        }}>
         Hola Mundo
+        <AntDesign name="stepforward" size={20}></AntDesign>
       </Text>
     </View>
   );
