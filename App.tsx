df095676a381896dd21830f2e984494252355241
dagmawi-y <dagmawi.inbox@gmail.com>
2023-07-12 19:54:02 +0300

    post component UI in app.tsx

--- App.tsx
+++ App.tsx
@@ -36,31 +36,64 @@ const App = () => {
       />
 
       {/* Footer */}
-      <View style={styles.iconContainer}>
-        <AntDesign
-          name={'hearto'}
-          size={24}
-          style={styles.icon}
-          color={colors.black}
-        />
-        <Ionicons
-          name="chatbubble-outline"
-          size={24}
-          style={styles.icon}
-          color={colors.black}
-        />
-        <Feather
-          name="send"
-          size={24}
-          style={styles.icon}
-          color={colors.black}
-        />
-        <Feather
-          name="bookmark"
-          size={24}
-          style={{marginLeft: 'auto'}}
-          color={colors.black}
-        />
+      <View style={styles.footer}>
+        <View style={styles.iconContainer}>
+          <AntDesign
+            name={'hearto'}
+            size={24}
+            style={styles.icon}
+            color={colors.black}
+          />
+          <Ionicons
+            name="chatbubble-outline"
+            size={24}
+            style={styles.icon}
+            color={colors.black}
+          />
+          <Feather
+            name="send"
+            size={24}
+            style={styles.icon}
+            color={colors.black}
+          />
+          <Feather
+            name="bookmark"
+            size={24}
+            style={{marginLeft: 'auto'}}
+            color={colors.black}
+          />
+        </View>
+
+        {/* Likes */}
+        <Text style={styles.text}>
+          Liked by <Text style={styles.bold}>dugudaga</Text> and{' '}
+          <Text style={styles.bold}>88 others</Text>
+        </Text>
+
+        {/* Post Description */}
+        <Text style={styles.text}>
+          <Text style={styles.bold}>silviadueng</Text> Lorem ipsum dolor sit,
+          amet consectetur adipisicing elit. Sunt quod fuga quaerat rem.
+          Consectetur officiis quod quaerat earum inventore quo veniam totam,
+          illo odit incidunt deserunt temporibus! Neque, ab sit? Quas ipsam
+          voluptatem labore impedit incidunt, eius fuga fugit minima velit
+          reiciendis. Distinctio deleniti porro a nam incidunt laudantium. Sed
+          facilis obcaecati saepe nemo et molestiae deserunt similique
+          doloremque aspernatur.
+        </Text>
+
+        {/* Comments */}
+        <Text>View all 16 comments</Text>
+        <View style={styles.comment}>
+          <Text style={styles.commentText}>
+            <Text style={styles.bold}>silviadueng</Text> Lorem ipsum dolor sit,
+            amet consectetur adipisicing elit.
+          </Text>
+          <AntDesign name={'hearto'} style={styles.icon} color={colors.black} />
+        </View>
+
+        {/* Posted Date */}
+        <Text>19 December, 2023</Text>
       </View>
     </View>
   );
@@ -91,12 +124,31 @@ const styles = StyleSheet.create({
   threeDots: {
     marginLeft: 'auto',
   },
+  footer: {
+    padding: 10,
+  },
   iconContainer: {
     flexDirection: 'row',
-    padding: 10,
+    marginBottom: 5,
   },
   icon: {
     marginHorizontal: 5,
+  },
+  text: {
+    color: colors.black,
+    lineHeight: 18,
+  },
+  bold: {
+    fontWeight: fonts.weight.bold,
+  },
+  commentText: {
+    color: colors.black,
+    flex: 1,
+    lineHeight: 18,
+  },
+  comment: {
+    flexDirection: 'row',
+    alignItems: 'center',
   },
 });
 
